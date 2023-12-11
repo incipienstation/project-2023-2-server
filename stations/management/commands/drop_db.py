05433930ea4261dbfa61f765e5541fa36998ad01
Jinan Jeong <jinanjeong@snu.ac.kr>
2023-12-12 06:18:52 +0900

    fix: fix drop_db command

--- stations/management/commands/drop_db.py
+++ stations/management/commands/drop_db.py
@@ -1,11 +1,15 @@
 from django.core.management.base import BaseCommand
 
-from stations.models import Station
+from stations.models import Station, Prediction
 
 
 class Command(BaseCommand):
     help = 'Drop database'
 
-    def handle(self):
-        Station.objects.all().delete()
-        self.stdout.write(self.style.SUCCESS('Data drop completed successfully.'))
+    def handle(self, *args, **options):
+        try:
+            Station.objects.all().delete()
+            Prediction.objects.all().delete()  # just in case
+            self.stdout.write(self.style.SUCCESS('Successfully deleted all Station instances'))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f'An error occurred: {e}'))
